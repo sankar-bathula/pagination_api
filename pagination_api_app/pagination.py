440807fbd261cbb1cababafaa698b1b8e5f8014b
himavardhank <himavardhank@gmail.com>
2019-02-02 11:27:57 +0530

    added pagination with CursorPagination and LimitOffsetPagination

--- pagination_api_app/pagination.py
+++ pagination_api_app/pagination.py
@@ -1,4 +1,4 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination, CursorPagination
 class MyPagination(PageNumberPagination):
 	page_size = 5 #max_number of records for page
 	page_query_param = 'mypage' # http://127.0.0.1:8000/api/?mypage=10
@@ -6,3 +6,13 @@ class MyPagination(PageNumberPagination):
 	max_page_size = 11
 	last_page_strings = ('end_page',)
 
+class MyPagination2(LimitOffsetPagination):
+	default_limit = 10
+	limit_query_parm = 'mylimit'#http://127.0.0.1:8000/api/?limit=5&offset=20
+	offset_query_parm = 'myoffset'
+	max_limit = 20
+	page_size = 10
+class MyPagination3(CursorPagination):
+	ordering = 'esal'
+	page_size = 8
+	cursor_query_param = 'mycursor'
--- pagination_api_app/views.py
+++ pagination_api_app/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from pagination_api_app.models import Employee
 from pagination_api_app.serializers import EmployeeSerializer
 from rest_framework.viewsets import ModelViewSet
-from pagination_api_app.pagination import MyPagination
+from pagination_api_app.pagination import MyPagination, MyPagination2, MyPagination3
 # Create your views here.
 class EmployeePaginationCRUD(ModelViewSet):
 	queryset = Employee.objects.all()
 	serializer_class = EmployeeSerializer
-	pagination_class = MyPagination
+	pagination_class = MyPagination3
